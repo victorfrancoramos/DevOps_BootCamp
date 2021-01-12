787ed13b67c9989bda4b5aace03d478a055a16f2
barondandi <barondandi@gmail.com>
2021-01-12 22:36:36 +0100

    Python code Update

--- REST_APIs_OCCM/Trident_Install.py
+++ REST_APIs_OCCM/Trident_Install.py
@@ -10,42 +10,42 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def login(req, base_url, username, password):
-    print("Obteniendo token...")
+    print("Getting token...")
 
     header = {"content-type": "application/json"}
 
-    # Obtenemos domain, audience y clientId necesarios para payload de autenticación
+    # We get domain, audience and clientId required for the authentication payload
     r = req.get(url="https://occm.demo.netapp.com:443/occm/api/occm/system/support-services", headers=header,
                 verify=False)
 
-    respuesta = r.json()
-    print("domain: ", respuesta["portalService"]["auth0Information"]["domain"])
-    domain = respuesta["portalService"]["auth0Information"]["domain"]
-    print("audience: ", respuesta["portalService"]["auth0Information"]["audience"])
-    audience = respuesta["portalService"]["auth0Information"]["audience"]
-    print("clientId: ", respuesta["portalService"]["auth0Information"]["clientId"])
-    clientId = respuesta["portalService"]["auth0Information"]["clientId"]
+    response = r.json()
+    print("domain: ", response["portalService"]["auth0Information"]["domain"])
+    domain = response["portalService"]["auth0Information"]["domain"]
+    print("audience: ", response["portalService"]["auth0Information"]["audience"])
+    audience = response["portalService"]["auth0Information"]["audience"]
+    print("clientId: ", response["portalService"]["auth0Information"]["clientId"])
+    clientId = response["portalService"]["auth0Information"]["clientId"]
 
     auth_url = "https://" + domain + "/oauth/token"
 
     auth_payload = {"grant_type": "password", "username": username, "password": password, "audience": audience,
                     "scope": "profile", "client_id": clientId}
 
-    # Hacemos POST para obtener token
+    # We issue a POST command to get the token
     a = req.post(url=auth_url, json=auth_payload, headers=header, verify=False)
 
-    respuesta = a.json()
-    token = respuesta["access_token"]
+    response = a.json()
+    token = response["access_token"]
 
     return token
 
 
 def get_k8s_clusters(req, token):
-    # Añadimos el token al header y hacemos get para extraer los clusters k8s ya registrados en OCCM
+    # We add the token to the header and issue a GET command to extract the k8s clusters already registered in OCCM
     hed = {'Authorization': 'Bearer ' + token}
     s = req.get(url="https://occm.demo.netapp.com/occm/api/k8s/clusters", headers=hed, verify=False)
 
-    # Formateamos la salida haciendo uso de la librería texttable
+    # We format the output making use of the texttable library
     ctr = 0
     clusters = s.json()
     tab = tt.Texttable()
@@ -61,13 +61,13 @@ def get_k8s_clusters(req, token):
         row = [clus, cep, ver, trid]
         tab.add_row(row)
         tab.set_cols_align(['c', 'c', 'c', 'c'])
-    print("clusters de K8s en instancia OCCM:{}".format(ctr))
+    print("K8s clusters in the OCCM instance:{}".format(ctr))
     setdisplay = tab.draw()
     print(setdisplay)
 
 
 def post_k8s_cluster(req, kubeconfig, contextname, token):
-    print("Registramos el K8s cluster => ", contextname)
+    print("Registering K8s cluster => ", contextname)
 
     payload = {"k8sconfig": kubeconfig, "contextNames": [contextname]}
     header_save = {"content-type": "application/json", 'Authorization': 'Bearer ' + token}
@@ -76,14 +76,14 @@ def post_k8s_cluster(req, kubeconfig, contextname, token):
 
 
 def install_trident(req, k8sCluster, ontapcluster, ips, token):
-    # Obtenemos kubernetes cluster id y workingenvironment id de los clusters pasados en los argumentos
+    # We get the kubernetes cluster id and workingenvironment id of the clusters passed in the arguments
     header_get = {"content-type": "application/json", 'Authorization': 'Bearer ' + token}
     r = req.get(url="https://occm.demo.netapp.com/occm/api/k8s/clusters", headers=header_get, verify=False)
 
-    respuesta = r.json()
+    response = r.json()
 
     k8sClusterId = "0"
-    for i in respuesta:
+    for i in response:
         if i["clusterName"] == k8sCluster:
             k8sClusterId = i["publicId"]
     print(k8sClusterId)
@@ -91,10 +91,10 @@ def install_trident(req, k8sCluster, ontapcluster, ips, token):
 
         r = req.get(url="https://occm.demo.netapp.com/occm/api/working-environments", headers=header_get, verify=False)
 
-        respuesta2 = r.json()
+        response2 = r.json()
 
         workingEnvironmentId = "0"
-        for i in respuesta2["onPremWorkingEnvironments"]:
+        for i in response2["onPremWorkingEnvironments"]:
             if i["name"] == ontapcluster:
                 workingEnvironmentId = i["publicId"]
 
@@ -108,42 +108,42 @@ def install_trident(req, k8sCluster, ontapcluster, ips, token):
             time.sleep(5)
 
         else:
-            print("No existe workingenvironment =>", ontapcluster)
+            print("workingenvironment non existent =>", ontapcluster)
     else:
-        print("No existe k8s clustert => ", k8sCluster)
+        print("k8s cluster non existent => ", k8sCluster)
 
 
 def main():
-    # Abrimos sesión
+    # Opening session
     s = requests.Session()
-    # Llamamos función login para autenticarnos y obtener token
+    # Calling login function to authenticate and get the token
     token = login(s, cfg.base_url, cfg.username, cfg.password)
 
-    # Abrimos kubeconfig file local y lo almacenamos como yaml dump
+    # Opening kubeconfig local file and storing it as yaml dump
     with open(cfg.kube_config_local) as file:
         kcl = yaml.load(file, Loader=yaml.FullLoader)
     kubeconfig_local = yaml.dump(kcl)
 
-    # Registramos cluster remote de K8s en OCCM
+    # Registering K8s local cluster in OCCM
     post_k8s_cluster(s, kubeconfig_local, cfg.k8s_local_context_name, token)
 
-    # Abrimos kubeconfig file remote y lo almacenamos como yaml dump
+    # Opening kubeconfig remote file and storing it as yaml dump
     with open(cfg.kube_config_remote) as file:
         kcr = yaml.load(file, Loader=yaml.FullLoader)
     kubeconfig_remote = yaml.dump(kcr)
 
-    # Registramos cluster remote de K8s en OCCM
+    # Registering K8s remote cluster in OCCM
     post_k8s_cluster(s, kubeconfig_remote, cfg.k8s_remote_context_name, token)
 
-    # Instalamos Trident en el cluster local de K8s
+    # Installing Trident in K8s local cluster
     install_trident(s, "k8s1-onPrem", "onPrem", "192.168.0.0/24", token)
 
-    # Instalamos Trident en el cluster remote de K8s
+    # Installing Trident in K8s remote cluster
     install_trident(s, "k8s2-remote", "remote", "192.168.0.0/24", token)
 
-    print("Verificando registro en OCCM...")
+    print("Verifyng OCCM registration...")
     time.sleep(10)
-    # Listamos los clusters de kubernetes vinculados a OCCM
+    # Listing kubernetes clusters linked to OCCM
     get_k8s_clusters(s, token)
 
 
