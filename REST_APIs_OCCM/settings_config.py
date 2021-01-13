638062f113bd9785e97b5da5ecae1b5296607f0e
barondandi <barondandi@gmail.com>
2021-01-13 13:47:08 +0100

    Python code Update

--- REST_APIs_OCCM/settings_config.py
+++ REST_APIs_OCCM/settings_config.py
@@ -1,3 +1,5 @@
 base_url = "https://occm.demo.netapp.com"
 username = "rt1510828@demo.netapp.com"
 password = "Netapp1!"
+audience = "https://api.cloud.netapp.com",
+client_id = "QC3AgHk6qdbmC7Yyr82ApBwaaJLwRrNO"
